426ca66f668d2ad5c67912efb9385a7951cfa876
S.P. Mohanty <spmohanty91@gmail.com>
2020-03-28 13:46:05 +0100

    Add a separate color config class

---
+++ colors.py
@@ -0,0 +1,30 @@
+class Colors:
+    """
+    Reference : https://materialuicolors.co/
+    # Level : 600
+
+    Can potentially use : https://github.com/secretBiology/SecretColors/
+    """
+    RED = (229, 57, 53)
+    PINK = (216, 27, 96)
+    PURPLE = (142, 36, 170)
+    DEEP_PURPLE = (94, 53, 177)
+    INDIGO = (57, 73, 171)
+    BLUE = (30, 136, 229)
+    LIGHT_BLUE = (3, 155, 229)
+    CYAN = (0, 172, 193)
+    TEAL = (0, 137, 123)
+    GREEN = (67, 160, 71)
+    LIGHT_GREEN = (124, 179, 66)
+    LIME = (192, 202, 51)
+    YELLOW = (253, 216, 53)
+    AMBER = (255, 179, 0)
+    ORANGE = (251, 140, 0)
+    DEEP_ORANGE = (244, 81, 30)
+    BROWN = (109, 76, 65)
+    GREY = (117, 117, 117)
+    BLUE_GREY = (84, 110, 122)
+
+if __name__ == "__main__":
+    colors = Colors()
+    # TODO setup a small pygame setup to display all the colors in the palette
