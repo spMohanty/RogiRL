06b8f8b5acfce8058c25791d23ec96031a2a15d2
S.P. Mohanty <spmohanty91@gmail.com>
2020-04-01 22:52:05 +0200

    Add user settable parameters

--- disease_sim/server.py
+++ disease_sim/server.py
@@ -12,6 +12,7 @@ except ImportError:
 
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid, ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
 
 COLOR_MAP = ColorMap()
 
@@ -72,5 +73,49 @@ model_kwargs = {
                     "height": grid_height,
                 }
 
+"""
+                    width=50, 
+                    height=50,
+                    n_agents=1000,
+                    n_vaccines=100,
+                    initial_infection_fraction=0.5,
+                    initial_vaccination_fraction=0.05,
+                    prob_infection=0.2,
+                    prob_agent_movement=0.0,
+                    disease_planner="simple_seir",
+                    max_timesteps=200,
+                    toric=True,
+                    seed = None
+"""
+model_params = {
+    "width" : 50,
+    "height" : 50,
+    "n_agents" : UserSettableParameter('number', 'No. of Agents', value=500),
+    "n_vaccines" : UserSettableParameter('number', 'Vaccine Budget', value=100),
+    "initial_infection_fraction" : \
+        UserSettableParameter(
+                'slider', 'Initial Infection Fraction', value=0.1,
+                min_value=0.01, max_value=0.99, step=0.01
+                ),
+    "initial_vaccination_fraction" : \
+        UserSettableParameter(
+                'slider', 'Initial Vaccination Fraction', value=0.05,
+                min_value=0.01, max_value=0.99, step=0.01
+                ),
+    "prob_infection" : \
+        UserSettableParameter(
+                'slider', 'Infection Probability', value=0.2,
+                min_value=0.01, max_value=1.0, step=0.01
+                ),
+    "prob_agent_movement" : \
+        UserSettableParameter(
+                'slider', 'Movement Probability', value=0.0,
+                min_value=0.00, max_value=1.0, step=0.01
+                ), 
+    "max_timesteps" : UserSettableParameter('number', 'Max. Timesteps', value=200),
+    "toric" : UserSettableParameter('checkbox', 'Toric Grid', value=True),
+    "seed" : UserSettableParameter('number', 'Seed', value=420),
+}
+
 server = ModularServer(DiseaseSimModel, [canvas_element, chart_element],  # noqa
-                       "DiseaseSim", model_kwargs)
+                       "Disease Simulator", model_params)
