cb4758f1f38dc8a4a31439bf71dd5156a08366cd
S.P. Mohanty <spmohanty91@gmail.com>
2020-04-09 05:42:12 +0200

    Addresses #9 - fixes linter errors in scheduler.py

--- rogi_rl/scheduler.py
+++ rogi_rl/scheduler.py
@@ -16,7 +16,7 @@ class CustomScheduler(RandomActivation):
     def add(self, agent: Agent) -> None:
         self._agents[agent.unique_id] = agent
         self._agent_state_index[agent.state][agent.unique_id] = agent
-    
+
     def remove(self, agent: Agent) -> None:
         del self._agents[agent.unique_id]
 
@@ -25,12 +25,15 @@ class CustomScheduler(RandomActivation):
                 del self._agent_state_index[state][agent.unique_id]
             except KeyError:
                 pass
-    
-    def update_agent_state_in_registry(self, agent: Agent, previous_state: AgentState) -> None:
+
+    def update_agent_state_in_registry(
+            self,
+            agent: Agent,
+            previous_state: AgentState) -> None:
         del self._agent_state_index[previous_state][agent.unique_id]
         self._agent_state_index[agent.state][agent.unique_id] = agent
 
-        ## Update the Model Observation 
+        # Update the Model Observation
         # self.model.observation[agent.pos]
         agent_x, agent_y = agent.pos
         self.model.observation[agent_x, agent_y, previous_state.value] = 0
@@ -45,4 +48,6 @@ class CustomScheduler(RandomActivation):
 
     def get_agent_fraction_by_state(self, state: AgentState) -> int:
         """ Returns the current number of agents in a particular state. """
-        return len(self._agent_state_index[state].keys()) / self.get_agent_count()    
+        _frac = \
+            len(self._agent_state_index[state].keys()) / self.get_agent_count()
+        return _frac
