3e2cc9faa17dd44927874412d84ca382f2032d7c
S.P. Mohanty <spmohanty91@gmail.com>
2020-04-13 05:44:47 +0200

    Addresses #4 - Setups placeholder for pytest

--- tests/test_rogi_rl.py
+++ tests/test_rogi_rl.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python
 
 """Tests for `rogi_rl` package."""
-
-import unittest
-from rogi_rl import RogiSimEnv
+from rogi_rl import RogiSimEnv  # noqa
 
 
-class TestRogiSimEnv(unittest.TestCase):
-    """Tests for `rogi_rl` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_env_instantiation(self):
-        """
-        Test that a newly instantiated env
-        returns a valid observation of the correct shape
-        """
-        config = {
-            "width": 50,
-            "height": 50
-        }
-        env = RogiSimEnv(config)
-        observation = env.reset()
-        assert observation.shape == (50, 50, 6)
+def test_placeholder():
+    """
+    Placeholder Test
+    """
+    assert True is True
